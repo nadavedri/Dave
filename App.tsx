d00cd0625d3daff93dc64e7c8bcb0b09dff88f68
NadavDive <nadav052433@gmail.com>
2024-10-11 21:33:22 +0300

    map with location

--- App.tsx
+++ App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Register, LoginScreen } from './src/screens';
-import { PaperProvider } from 'react-native-paper';
-import { DrawerNavigator } from './src/navigators';
+import { Register, LoginScreen, AppendDive, DivingMap } from "./src/screens";
+import { PaperProvider } from "react-native-paper";
+import React, { useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export const App = () => {
+
   return (
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="DivingMap" component={DivingMap} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="MainApp" component={DrawerNavigator} />
--- src/screens/DivingMap.tsx
+++ src/screens/DivingMap.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
 export const DivingMap = () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location); 
+    })();
+  }, []);
+
   return (
     <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    >
-      <Marker
-        coordinate={{
+        style={{ flex: 1 }}
+        initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
         }}
-        title="Marker Title"
-        description="Marker Description"
-      />
+        region={location ? {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        } : undefined}
+      >
+      {location && (
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      )}
     </MapView>
   );
 };
