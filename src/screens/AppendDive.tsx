88f269398439693340619bddbe939c1bafa2f8ce
Naos <ouriya124@gmail.com>
2024-10-11 23:11:30 +0300

    trying to make it work

--- src/screens/AppendDive.tsx
+++ src/screens/AppendDive.tsx
@@ -1,13 +1,24 @@
-import { HomeNavbar, BottomAddDive } from '../components';
-import React, { useState } from "react";
-import { View, StyleSheet } from 'react-native';
+import { auth } from '../firebaseConfig';
+import { HomeNavbar, BottomAddDive, DaveDrawer } from '../components';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from 'react-native';
 
 export const AppendDive = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const [email, setEmail] = useState('')
 
-  return (
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setEmail(user?.email ?? "");
+    } else {
+      console.log('No user is signed in');
+    }
+  }, []);
+  return (  
     <View style={styles.container}>
-      {isDrawerOpen ?? }
+      {isDrawerOpen && <DaveDrawer/>}
+      <Text >hello {email}</Text>
       <View style={styles.topPortion}>
         <HomeNavbar setIsDrawerOpen={setIsDrawerOpen}/>
 s      </View>
