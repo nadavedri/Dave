08d5249a91ba26e3fc90bac1bf60052bb3fabeaf
NadavDive <nadav052433@gmail.com>
2024-10-12 17:35:24 +0300

    Lets GO!

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { Register, LoginScreen, AppendDive, DivingMap } from "./src/screens";
+import { Register, LoginScreen } from "./src/screens";
 import { PaperProvider } from "react-native-paper";
-import React, { useState } from "react";
+import { DrawerNavigator } from "./src/navigators";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -14,10 +14,10 @@ export const App = () => {
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="DivingMap" component={DivingMap} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="MainApp" component={DrawerNavigator} />
+          
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
